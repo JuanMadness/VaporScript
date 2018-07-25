e2778f45bfb952cd22dc312e4c8f0d4b4d9eda34
JuanMadness <sebastianrenzer@gmail.com>
2018-07-25 20:18:57 +0200

    -little bit more code

--- VaporScript.sh
+++ VaporScript.sh
@@ -3,14 +3,18 @@
 
 #parameter
 ########
-
+vaporVersion=18-07-25
 vaporFolder=vapor
 vaporGameList=$vaporFolder/vaporGameList.tmp
+vaporAddGameList=$vaporFolder/vaporAddGameList.txt
 vaporConfig=$vaporFolder/vapor.conf
 vaporLog=$vaporFolder/vapor.log
+
+steamLib=.
+steamcmdPath=
 user=spoker14
 userPw=
-sleepTime=6h
+sleepTime=12h
 
 #functions
 #######
@@ -20,28 +24,39 @@ function log {
 	printf "%-30s%s\n" "$(date)" "$1"
 }
 
+function closedScript {
+	log "Check https://github.com/JuanMadness/VaporScript for updates!"
+	log "Closed VaporScript $vaporVersion"
+	exit
+}
+
 function loadConfig {
-	echo nothing
+	log "Loading config..."
+	log "FEATURE NOT IMPLEMENTED :("
 }
 
 #main
 #####
+trap closedScript EXIT SIGINT
 
 echo "vaporScript"
-log "vaporScript 18-07-25"
+log "vaporScript $vaporVersion"
 
 mkdir -p $vaporFolder
 touch $vaporGameList
 touch $vaporConfig
 touch $vaporLog
+touch $vaporAddGameList
 
 while [ true ]
 do
+	loadConfig
+
 	log "Starting update check..."
 	cmd="steamcmd +login $user"
 	echo "" > $vaporGameList
 
-	grep -rnw 'appid' *.acf | grep -Eo '[0-9]{2,10}' | while read line
+	grep -rnw 'appid' $steamLib/*.acf | grep -Eo '[0-9]{2,10}' | while read line
 	do
 		echo " $line" >> $vaporGameList
 	done 
